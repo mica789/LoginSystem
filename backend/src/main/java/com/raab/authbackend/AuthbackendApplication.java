30aff767dd16de2057555411682f5d02710edee0
Mica Raab <mica.raab@post.runi.ac.il>
2025-07-06 13:02:21 +0300

    Initial fullstack project setup

---
+++ backend/src/main/java/com/raab/authbackend/AuthbackendApplication.java
@@ -0,0 +1,13 @@
+package com.raab.authbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthbackendApplication.class, args);
+	}
+
+}
